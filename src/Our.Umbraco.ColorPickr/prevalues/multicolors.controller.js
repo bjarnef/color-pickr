a142883025892040452bec6e9c468cc4688e9b48
Bjarne Fyrstenborg <bf@oerskov.dk>
2019-08-04 21:14:26 +0200

    Add prevalue editor to select predefined color swatches

---
+++ src/Our.Umbraco.ColorPickr/prevalues/multicolors.controller.js
@@ -0,0 +1,77 @@
+angular.module("umbraco").controller("Our.Umbraco.ColorPickr.PrevalueEditors.MultiColorsController", function ($scope, assetsService, localizationService, angularHelper) {
+
+    const vm = this;
+
+    vm.cancel = cancel;
+    vm.save = save;
+
+    vm.add = add;
+    vm.remove = remove;
+
+    function init() {
+        
+        $scope.hasError = false;
+        $scope.focusOnNew = false;
+
+        if (!$scope.model.value) {
+            $scope.model.value = [];
+        }
+
+        $scope.color = $scope.color || null;
+    }
+
+    function cancel(values) {
+        console.log("cancel", values);
+    }
+
+    function save(color) {
+        console.log("save", color);
+        $scope.color = color;
+    }
+
+    function remove(item, evt) {
+        evt.preventDefault();
+
+        $scope.model.value = _.reject($scope.model.value, function (x) {
+            return x.value === item.value;
+        });
+        
+    };
+
+    function add(evt) {
+        evt.preventDefault();
+
+        console.log("$scope.color", $scope.color);
+        
+        if ($scope.color) {
+            var exists = _.find($scope.model.value, function(item) {
+                return item.value.toUpperCase() === $scope.color.toUpperCase();
+            });
+
+            if (!exists) {  
+                $scope.model.value.push({ value: $scope.color });
+
+                $scope.hasError = false;
+                $scope.focusOnNew = true;
+                return;
+            }
+        }
+
+        //there was an error, do the highlight (will be set back by the directive)
+        $scope.hasError = true;            
+    };
+
+    $scope.sortableOptions = {
+        axis: 'y',
+        containment: 'parent',
+        cursor: 'move',
+        items: '> div.control-group',
+        tolerance: 'pointer',
+        update: function (e, ui) {
+            angularHelper.getCurrentForm($scope).$setDirty();
+        }
+    };
+
+    init();
+
+});
